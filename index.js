845210e8358dee5396fcd329d418fefe123143bf
Densyy <densyy@hotmail.com>
2023-09-19 05:38:44 -0300

    adiciona index

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = {}
+/* ---- Requires ---- */
+
+const Validator = require('validatorjs')
+
+/* ---- Helpers ---- */
+
+const language = require('./assets/lang/pt-br')
+const firstFn = require('./assets/prototype/first')
+
+/* ---- Default ---- */
+
+Validator.setMessages('en', language)
+Validator.prototype.first = firstFn
+
+/* ---- Exports ---- */
+
+module.exports = Validator
